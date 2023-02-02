c62a628acde50d478ad0e0bd1639ee76e95680a1
wilmays10 <wilmays10@gmail.com>
2023-02-02 17:38:06 -0300

    feat: iniciando proyecto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cattler
 backend challenge
+
+## Tecnología usada
+Desarrollado con python 3.10.6 + django 4.1.2. Para el desarrollo de API's se
+usó django rest framework 3.14.0. 
---
+++ init_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# entorno virtual
+sudo apt update
+sudo apt install python3-pip
+python3 -m venv env
+source env/bin/activate
+
+# instalar dependencias
+pip3 install -r requirements.txt
+
+# Creacion base de datos
+python manage.py makemigrations
+python manage.py migrate
+
+# Carga de datos de ejemplo
+#python manage.py loaddata init_data/db.json
+
+# Inicializar server local
+python manage.py runserver
